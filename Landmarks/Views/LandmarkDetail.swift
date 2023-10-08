21ca4ee4dd9ba474b2722548e044a6a20f1eaf3a
Abdelrahman Esmail <pro.abdelrahmanismail@gmail.com>
2023-10-08 16:23:17 +0300

    Building lists and navigation

---
+++ Landmarks/Views/LandmarkDetail.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkDetail.swift
+//  Landmarks
+//
+//  Created by Abdelrahman Esmail on 08/10/2023.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkDetail()
+}
---
+++ Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Abdelrahman Esmail on 08/10/2023.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkRow()
+}
