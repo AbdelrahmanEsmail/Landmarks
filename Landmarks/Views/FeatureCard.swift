0d789f4f86a56da3aa56ef28c5ae26db8cf4549c
Abdelrahman Esmail <pro.abdelrahmanismail@gmail.com>
2023-10-12 03:50:59 +0300

    Create a View to Represent a UIPageViewController

---
+++ Landmarks/Views/FeatureCard.swift
@@ -0,0 +1,18 @@
+//
+//  FeatureCard.swift
+//  Landmarks
+//
+//  Created by Abdelrahman Esmail on 11/10/2023.
+//
+
+import SwiftUI
+
+struct FeatureCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FeatureCard()
+}
---
+++ Landmarks/Views/PageViewController.swift
@@ -0,0 +1,8 @@
+//
+//  PageViewController.swift
+//  Landmarks
+//
+//  Created by Abdelrahman Esmail on 11/10/2023.
+//
+
+import Foundation
